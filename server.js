9bd101ceb4ca54280e49cc9b23a71b335a47607c
Daniel Mattox <dmattox10@gmail.com>
2020-02-24 15:01:34 -0500

    Edited auth route, server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const userRouter = require('./routes/users')
 const authRouter = require('./routes/auth')
 
+const auth = require('./middleware/auth')
 const configure = require('./middleware/config')
 
 const server = express()
@@ -10,6 +11,6 @@ const server = express()
 configure(server)
 
 server.use('/api/auth', authRouter)
-server.use('/api/users', userRouter)
+server.use('/api/users', auth, userRouter)
 
 module.exports = server
