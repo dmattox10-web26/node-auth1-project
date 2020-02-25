3b5342e0643888bc77f0b578d50cd2a29154b3fc
Daniel Mattox <dmattox10@gmail.com>
2020-02-25 13:39:06 -0500

    Tested, working

--- middleware/config.js
+++ middleware/config.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session')
+const KnexStore = require('connect-session-knex')(session)
+const knex = require('../database/db-config') // needed for storing sessions in the db
 
 const sessionConfig = {
   name: 'monster',
@@ -12,7 +14,16 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 10, // 10 minutes
     secure: false, // should be true in prod
     httpOnly: true, // prevents js access
-  }
+  },
+  store: new KnexStore({
+    knex,
+    tablename: 'sessions',
+    createtable: true,
+    sidfieldname: 'sid',
+    clearInterval: 1000 * 60 * 15,
+
+  }), //knex
+
 }
 module.exports = server => {
   server.use(helmet());
